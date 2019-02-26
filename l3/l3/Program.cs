191761dafec78efc37b04fe9fd5a8f2eb9e1ed1d
Bulat <yaremakadanil@gmail.com>
2019-02-26 12:31:07 +0700

    final commit

--- l3/l3/Program.cs
+++ l3/l3/Program.cs
@@ -9,11 +9,28 @@ namespace l3
     class Program
     {
         static void Main(string[] args)
-        {         
-           case 4:
-            int raz = a - b;
-            Console.WriteLine("{0}", raz);
-            break;
+        {
+            int i;
+
+            {
+
+                {
+                    Console.Write("Меню:\n1) Введите A \n2) Введите B \n3) Сложение \n4) Вычитание \n5) Умножение \n6) Деление \n7) Выход");
+                    i = int.Parse(Console.ReadLine());
+                    switch (i)
+                    {
+
+
+                       
+                        default:
+                            Console.WriteLine("");
+                            break;
+                    }
+                    Console.Write("\nНажмите любую клавишу...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
         }
     }
 }
