a66d602fee75ac123c54ea05b60c8719ccd2c365
Bulat <yaremakadanil@gmail.com>
2019-02-26 12:39:53 +0700

    comit

--- l3/l3/Program.cs
+++ l3/l3/Program.cs
@@ -9,38 +9,28 @@ namespace l3
     class Program
     {
         static void Main(string[] args)
-
-
         {
+            int i;
 
-             case 1:
-             Console.WriteLine("Введите A");
-            int a = int.Parse(Console.ReadLine());
-            break;
+            {
 
-           case 2:
-            Console.WriteLine("Введите B");
-            int b = int.Parse(Console.ReadLine());
-             case 3:
-            int sum = a + b;
-            Console.WriteLine("{0}", sum);
-       
-           case 4:
-            int raz = a - b;
-            Console.WriteLine("{0}", raz);
+                {
+                    Console.Write("Меню:\n1) Введите A \n2) Введите B \n3) Сложение \n4) Вычитание \n5) Умножение \n6) Деление \n7) Выход");
+                    i = int.Parse(Console.ReadLine());
+                    switch (i)
+                    {
 
 
-        
-            case 6:
-            int del = a / b;
-            Console.WriteLine("{0}", del);
-        
-
-            case 5:
-            int pro = a * b;
-            Console.WriteLine("{0}", pro);
-
-            break;
+                       
+                        default:
+                            Console.WriteLine("");
+                            break;
+                    }
+                    Console.Write("\nНажмите любую клавишу...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
         }
     }
 }
